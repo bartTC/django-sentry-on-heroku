2b96241c175b97cedd9d50e45e5b3869e6983188
Martin Mahner <martin@mahner.org>
2016-07-08 11:21:29 +0200

    Redis Broker

--- sentry.conf.py
+++ sentry.conf.py
@@ -66,8 +66,9 @@ SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
 # information on configuring your queue broker and workers. Sentry relies
 # on a Python framework called Celery to manage queues.
 
+
 CELERY_ALWAYS_EAGER = False
-#BROKER_URL = os.environ.get('RABBITMQ_BIGWIG_URL', '')
+BROKER_URL = os.environ.get('REDIS_URL') + '/0'
 
 #################
 ## Rate Limits ##
